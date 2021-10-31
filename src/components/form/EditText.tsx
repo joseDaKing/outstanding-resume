925855fb597ce21f53615769afa6fa7863a79bdb
Yousef Abdulkarim <38055561+joseDaKing@users.noreply.github.com>
2021-10-31 13:35:54 +0100

    Used the PlaceAside component in EditText and moved ContentEditable and EditText component to form directory

---
+++ src/components/form/EditText.tsx
@@ -0,0 +1,151 @@
+import React from "react";
+
+import { Box, PlaceAside } from "../layout";
+
+import type { KeyboardEvent } from "react";
+
+import { useEffect, useRef, useState } from "react";
+
+import { MdEdit, MdLoop } from "react-icons/md";
+
+import { css, styled } from "../../stitches";
+
+import { ContentEditable } from "./ContentEditable";
+
+import OutsideClickHandler from "react-outside-click-handler";
+
+import { IconContainer, IconGroupHover } from "../primitives";
+
+interface IEditTextProps {
+    value?: string;
+    resetable?: string;
+    onChange?: (value: string) => void;
+    left?: () => JSX.Element;
+    right?: () => JSX.Element;
+}
+
+const ContentEditableStyles = css({
+    display: "inline-block",
+    borderBottomWidth: "$2",
+    _borderBottomColor: "$light-blue-500",
+    _borderBottomOpacity: 0,
+    transitionDuration: "$100",
+    transitionTimingFunction: "$inOut",
+    "&:focus": {
+        outline: "none",
+        _borderBottomOpacity: 1,
+    }
+});
+
+const EditTextIconContainer = styled(IconContainer, {
+    cursor: "pointer"
+});
+
+export const EditText: React.FC<IEditTextProps> = ({ value, resetable, onChange, children, left, right }) => {
+
+    const contentEditableRef = useRef<HTMLDivElement>(null);
+
+    const [text, setText] = useState(value ?? "");
+
+    useEffect(() => {
+        
+        if (onChange) {
+
+            onChange(text);
+        }
+    /* eslint-disable */
+    }, [text]);
+    /* eslint-endable */
+
+    const [isDisabled, setIsDisabled] = useState(true);
+
+    useEffect(() => {
+        
+        if (!isDisabled && contentEditableRef.current) {
+
+            contentEditableRef.current.focus();
+
+            const selection = getSelection();
+
+            if (selection) {
+
+                selection.removeAllRanges();
+
+                selection.selectAllChildren(contentEditableRef.current);
+            }
+        }
+    /* eslint-disable */
+    }, [isDisabled]);
+    /* eslint-enable */
+
+    const onResetHandler = () => {
+
+        if (resetable && text !== resetable) {
+
+            setText(resetable);
+
+            setIsDisabled(true);
+        }
+    }
+
+    const onEditModeHandler = () => setIsDisabled(false);
+
+    const toDisabled = () => setIsDisabled(true);
+
+    const onKeyDownHandler = (event: KeyboardEvent<any>) => {
+
+        if (event.key === "Enter") {
+
+            setIsDisabled(true);
+        }
+    }
+
+    return (
+        <Box 
+        inline>
+            <IconGroupHover inline>
+                <Box css={{ height: 0.1 }}/>
+                
+                {left &&
+                <PlaceAside align="start">
+                    {left()}
+                </PlaceAside>}                
+
+                <OutsideClickHandler
+                display="inline-block"
+                onOutsideClick={toDisabled}>
+                    <ContentEditable
+                    className={ContentEditableStyles()}
+                    disabled={isDisabled}
+                    innerRef={contentEditableRef}
+                    onBlur={toDisabled}
+                    onKeyDown={onKeyDownHandler}
+                    onChange={event => setText(event.target.value)}
+                    value={text}/>
+                </OutsideClickHandler>
+
+                <PlaceAside align="end">
+                    <EditTextIconContainer
+                    hover
+                    css={{
+                        paddingLeft: "$2",
+                    }}
+                    onClick={onEditModeHandler}>
+                        <MdEdit/>
+                    </EditTextIconContainer>
+
+                    {resetable !== text && 
+                    <EditTextIconContainer
+                    hover
+                    onClick={onResetHandler}>
+                        <MdLoop/>
+                    </EditTextIconContainer>}
+
+                    {right && right()}
+                </PlaceAside>
+
+                <Box css={{ height: 0.1 }}/>
+            </IconGroupHover>
+        </Box>
+    );
+}
--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -3,3 +3,7 @@ export * from "./TextField";
 export * from "./TextArea";
 
 export * from "./Button";
+
+export * from "./EditText";
+
+export * from "./ContentEditable";
