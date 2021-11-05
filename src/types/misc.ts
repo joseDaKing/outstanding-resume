2b230ccee81f8e5a88a46b7c60fec056fa3f3d0d
Yousef Abdulkarim <38055561+joseDaKing@users.noreply.github.com>
2021-11-05 20:29:17 +0100

    Added Items types

--- src/types/misc.ts
+++ src/types/misc.ts
@@ -5,3 +5,5 @@ import ReactPDF from "@react-pdf/renderer";
 export type ArrayValues<T extends readonly any[]> = T[Extract<keyof T, `${number}`>];
 
 export type PDFTemplate = ReactElement<ReactPDF.DocumentProps, string | JSXElementConstructor<any>>;;
+
+export type Items = { items: Record<string, any> };
