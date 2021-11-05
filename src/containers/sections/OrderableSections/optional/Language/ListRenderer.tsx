547b29e2d6265fe6c4aaf4ba7a35f39ca1a061c2
Yousef Abdulkarim <38055561+joseDaKing@users.noreply.github.com>
2021-11-06 00:48:50 +0100

    Implemented the language section

--- src/containers/sections/OrderableSections/optional/Language/ListRenderer.tsx
+++ src/containers/sections/OrderableSections/optional/Language/ListRenderer.tsx
@@ -1,9 +1,53 @@
 import React from "react";
 
-export const ListRenderer: React.FC = () => {
+import { Stack } from "../../../../../components/layout";
+
+import { TextField, Level, LevelValue } from "../../../../../components/form";
+
+import { Accordion } from "../../../../../components/misc";
+
+import { 
+    useAppDispatch,  
+    language as languageSlice,
+    ILanguageItem
+}
+from "../../../../../store";
+
+import { IID } from "../../../../../types";
+
+export const ListRenderer: React.FC<ILanguageItem & IID> = ({ id, language, level }) => {
+
+    const dispatch = useAppDispatch();
+
+    const onNameChange = (language: string) => dispatch(languageSlice.actions.change([id, {
+        language
+    }]));
+
+    const onLevelChange = (level: LevelValue) => dispatch(languageSlice.actions.change([id, {
+        level
+    }]));
 
     return (
-        <>
-        </>
+        <Accordion.Item 
+        id={id}
+        title={language || "(Ej specificerat)"}>
+            <Stack 
+            axis="y"
+            space="md">
+                <Stack 
+                axis="x"
+                space="md">
+                    <TextField
+                    value={language}
+                    label="Språk"
+                    onChange={onNameChange}/>
+    
+                    <Level
+                    label="Nivå"
+                    value={level}
+                    onChange={onLevelChange}/>
+                </Stack>
+            </Stack>
+        </Accordion.Item>
     );
 }
