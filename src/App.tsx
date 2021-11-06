f90f99ac4e64b1d06ecc3544f189e36cf24b2b4f
Yousef Abdulkarim <38055561+joseDaKing@users.noreply.github.com>
2021-11-06 20:24:53 +0100

    added ResumeEditor page to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
-import React from "react";
-
-import { Stack, Box } from "./components/layout";
-
-import { Accordion } from "./components/misc";
-
 import "./globalStyles";
 
-export const App: React.FC = () => {
-    return (
-        <Accordion>
-            <Stack 
-            axis="x" 
-            css={{ 
-                _size: "$screen" 
-            }}>
-                <Box
-                css={{
-                    backgroundColor: "white",
-                    overflowY: "scroll",
-                    padding: "$10"
-                }}>
-                    
-                </Box>
+import React from "react";
 
-                <Box 
-                css={{
-                    backgroundColor: "$blue-gray-400"
-                }}>
-                    
-                </Box>
-            </Stack>
-        </Accordion>
-    );
-}
+import { ResumeEditor } from "./pages";
+
+export const App: React.FC = () => <ResumeEditor/>
 
 export default App;
