35e42ed9b20152b4931fdcf0f7268dfe54100799
Yousef Abdulkarim <38055561+joseDaKing@users.noreply.github.com>
2021-11-08 15:44:13 +0100

    Added a new type

--- src/types/misc.ts
+++ src/types/misc.ts
@@ -2,6 +2,8 @@ import type { ReactElement, JSXElementConstructor } from "react";
 
 import ReactPDF from "@react-pdf/renderer";
 
+import { IOnChange, IValue } from "./Props";
+
 export type ArrayValues<T extends readonly any[]> = T[Extract<keyof T, `${number}`>];
 
 export type PDFTemplate = ReactElement<ReactPDF.DocumentProps, string | JSXElementConstructor<any>>;
@@ -9,3 +11,5 @@ export type PDFTemplate = ReactElement<ReactPDF.DocumentProps, string | JSXEleme
 export type Simplify<T> = {
     [K in keyof T]: T[K];
 }
+
+export type Bind<T extends Object> = <K extends keyof T>(key: K) =>  IValue<T[K]> & IOnChange<T[K]>;
