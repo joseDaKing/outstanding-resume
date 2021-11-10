9ac5bb42eb53176f37eeb55a76a87fd2aabce4e5
Yousef Abdulkarim <38055561+joseDaKing@users.noreply.github.com>
2021-11-10 16:26:39 +0100

    Added a new type

--- src/types/misc.ts
+++ src/types/misc.ts
@@ -13,3 +13,5 @@ export type Simplify<T> = {
 }
 
 export type Bind<T extends Object> = <K extends keyof T>(key: K) =>  Required<IValue<T[K]> & IOnChange<T[K]>>;
+
+export type Get<T, K extends string> = K extends keyof T ? T[K] : never;
