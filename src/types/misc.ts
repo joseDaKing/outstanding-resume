dc96751d25d16f22e2e3f4b6e1651a211cf259b4
Yousef Abdulkarim <38055561+joseDaKing@users.noreply.github.com>
2021-11-11 03:49:34 +0100

    Removed some types

--- src/types/misc.ts
+++ src/types/misc.ts
@@ -3,27 +3,8 @@ import type { ReactElement, JSXElementConstructor } from "react";
 import ReactPDF from "@react-pdf/renderer";
 
 import { IOnChange, IValue } from "./Props";
-import { Merge } from "type-fest";
 
 export type ArrayValues<T extends readonly any[]> = T[Extract<keyof T, `${number}`>];
-
-type PDFJSX = (
-    ReactElement<
-        ReactPDF.DocumentProps, 
-        string 
-        | JSXElementConstructor<any>
-    > 
-    | null 
-    | undefined
-);
-
-type PDFComponentPropsWithChildren<T extends {}> = Merge<{ children: PDFJSX; }, T>;
-
-export type PDFComponent<T extends {} = {}> = (props: PDFComponentPropsWithChildren<T>) => PDFJSX;
-
-export type Simplify<T> = {
-    [K in keyof T]: T[K];
-}
 
 export type Bind<T extends Object> = <K extends keyof T>(key: K) =>  Required<IValue<T[K]> & IOnChange<T[K]>>;
 
