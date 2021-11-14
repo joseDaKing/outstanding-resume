b4f43a1d996e589dd2da4123def6e74f03f99295
Yousef Abdulkarim <38055561+joseDaKing@users.noreply.github.com>
2021-11-14 21:24:51 +0100

    Changed the code

--- src/pages/ResumeEditor/View.tsx
+++ src/pages/ResumeEditor/View.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useMemo } from "react";
+
+import { useDebouncedValue } from "rooks";
+
+import { PDFView } from "../../components/misc";
+
+import { Berlin } from "../../resumes";
+
+import { useAppSelector } from "../../store";
 
 export const View: React.FC = () => {
 
+    const store = useAppSelector(store => store);
+
+    const [debouncedStore] = useDebouncedValue(store, 1000, {
+        initializeWithNull: false
+    });
+
+    const template = useMemo(() => {
+        return <Berlin {...(debouncedStore as any)}/>
+    }, [JSON.stringify(debouncedStore)]);
+
     return (
-        <>
-        </>
+        <PDFView
+        document={template}/>
     );
 }
