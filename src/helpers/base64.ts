d3460a40f8b97e589259f4e7d0af28ca06940033
Yousef Abdulkarim <38055561+joseDaKing@users.noreply.github.com>
2022-05-12 00:26:08 +0200

    Fixed a minor issue

--- src/helpers/base64.ts
+++ src/helpers/base64.ts
@@ -1,6 +1,6 @@
 export const blobToBase64 = (blob: Blob) => new Promise<string>((resolve, reject) => {
     
-    const reader = new FileReader;
+    const reader = new FileReader();
     
     reader.onerror = () => reject();
     
